c6834f71140448b7f31a1037da21c339de8727c4
Tayeb Ahmed <atayeb.english939@gmail.com>
2024-05-22 19:55:11 +0600

    SQL Constraints

---
+++ SQL Constraints/NOT NULL.sql
@@ -0,0 +1,17 @@
+# Syntax:
+/*
+CREATE TABLE table_name (
+    column1 datatype NOT NULL,
+    column2 datatype NOT NULL,
+    ...
+);
+ */
+
+# Example:
+
+CREATE TABLE customers
+(
+    id   INT         NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    age  INT         NOT NULL
+);
